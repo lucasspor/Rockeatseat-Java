d3b232e7c248d1d5928af181fe963a2120dd91d1
lucasspor <lucassporto.contato@gmail.com>
2024-06-18 21:35:08 -0300

    feat: adding filter for auth

---
+++ todolist/src/main/java/br/com/lucasporto/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,40 @@
+package br.com.lucasporto.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter{
+
+  @SuppressWarnings("null")
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+      throws ServletException, IOException {
+       var authorization = request.getHeader("Authorization");
+       var authEncode =  authorization.substring("Basic".length()).trim();
+
+       byte[] authDecode = Base64.getDecoder().decode(authEncode);
+
+      var authString = new String(authDecode);
+
+       
+      
+       String[] credentials = authString.split(":");
+       String username = credentials[0];
+       String password = credentials[1];
+
+       System.out.println("Authorization");
+       System.out.println(username);
+       System.out.println(password);
+       chain.doFilter(request, response);
+  }
+  
+}
